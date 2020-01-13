f6b1964a477a54eb2980dd6504d2b551072414c4
Carl McCabe <carl.h.mccabe@gmail.com>
2020-01-13 14:41:53 +1000

    update session

--- router/auth_routes.js
+++ router/auth_routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { register, login, logout } = require("../controllers/auth_contoller");
+const { register, login, logout, activeUserSession } = require("../controllers/auth_contoller");
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", logout);
+router.get("/user", activeUserSession);
 
 module.exports = router;
